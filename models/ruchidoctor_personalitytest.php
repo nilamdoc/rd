b5e346779d9308eb4d873fba4f08740faf0bc3d6
Nilam Doctor <nilamdoc@gmail.com>
2024-09-07 19:50:17 +0530

    dede

--- models/ruchidoctor_personalitytest.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\models;
-
-class ruchidoctor_personalitytest extends \lithium\data\Model {
-	protected $_meta = array(
-		'connection' => 'defaultMySQL' 
-	);
-}
-?>
--- models/ruchidoctor_users.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\models;
-
-class ruchidoctor_users extends \lithium\data\Model {
-	protected $_meta = array(
-		'connection' => 'defaultMySQL' 
-	);
-}
-?>
