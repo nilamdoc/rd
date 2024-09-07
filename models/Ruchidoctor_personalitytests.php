5ed7610c85f20872694b151011a97d8200955b8f
Nilam Doctor <nilamdoc@gmail.com>
2024-09-07 19:07:45 +0530

    R r

--- models/Ruchidoctor_personalitytests.php
+++ models/Ruchidoctor_personalitytests.php
@@ -3,7 +3,7 @@ namespace app\models;
 
 class ruchidoctor_personalitytests extends \lithium\data\Model {
 	protected $_meta = array(
-		'connection' => 'defaultMySQL'
+		'connection' => 'defaultMySQL' 
 	);
 }
 ?>
--- models/Ruchidoctor_users.php
+++ models/Ruchidoctor_users.php
@@ -3,7 +3,7 @@ namespace app\models;
 
 class ruchidoctor_users extends \lithium\data\Model {
 	protected $_meta = array(
-		'connection' => 'defaultMySQL'
+		'connection' => 'defaultMySQL 
 	);
 }
 ?>
