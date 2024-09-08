6e060e3d4c7999ae301bbd390e2a5da548a21cb1
Nilam Doctor <nilamdoc@gmail.com>
2024-09-09 00:32:23 +0530

    ETL

--- views/pages/home.html.php
+++ views/pages/home.html.php
@@ -1,2 +1,14 @@
 <?php
 ?>
+<div>
+	<img src="img/LEAD/ETL.png" width="600px">
+	<hr>
+	<img src="img/LEAD/STL.png" width="600px">
+	<hr>
+	<img src="img/LEAD/NTL.png" width="600px">
+	<hr>
+	<img src="img/LEAD/ATL.png" width="600px">
+	<hr>
+	<img src="img/LEAD/CTL.png" width="600px">
+	<hr>
+</div>
