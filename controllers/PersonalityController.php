6821a58655715fa591f47ca74035c206deca5edc
Nilam Doctor <nilamdoc@gmail.com>
2024-09-25 13:03:28 +0530

    per

--- controllers/PersonalityController.php
+++ controllers/PersonalityController.php
@@ -16,9 +16,6 @@ class PersonalityController extends \lithium\action\Controller {
 
 		
 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: PUT, GET, POST");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 //		$this->_render['layout'] = 'coach';
 	}
@@ -26,7 +23,6 @@ class PersonalityController extends \lithium\action\Controller {
 	public function index(){
 		
 		$questions = Ruchidoctor_personalitytests::find('all',array('order'=>array('question'=>'ASC'))); 
-		print_r($questions);
 		return $this->render(array('json' => array("success"=>"Yes",'questions'=>$questions)));
 		
 	}
