c37b7564119b582d93b284974144e8d8caf31956
Nilam Doctor <nilamdoc@gmail.com>
2024-09-30 20:20:32 +0530

    Who

--- views/whoyouaretest/index.html.php
+++ views/whoyouaretest/index.html.php
@@ -1,8 +1,12 @@
 <div style="background-image:url('../img/RDimage.png'); background-repeat: no-repeat;
-  background-attachment: fixed;  background-position: center;
-  background-size: cover;
+  background-attachment: fixed;
+
 height:800" class="text-align-center ">
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <img src="../img/RuchiDoctor.png"><br><br>
 <img src="../img/WhoYouAre.png"><br><br><br><br>
+<div class="text-align-center"><form method="post" action="/whoyouaretest/reqister">
+	
+	<input type="Email" placeholder="Email" style="background-color:red; ">
+</form></div>
 </div>
