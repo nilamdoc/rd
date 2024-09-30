9703a45515af1a7b079d05c14d1dbae747adb3a2
Nilam Doctor <nilamdoc@gmail.com>
2024-09-30 19:47:06 +0530

    who

--- views/whoyouaretest/index.html.php
+++ views/whoyouaretest/index.html.php
@@ -1,2 +1,5 @@
-<div style="background-image:url('../img/RDimage.png');height:1000px">
+<div style="background-image:url('../img/RDimage.png');height:1000px" class="text-align-center ">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<img src="../img/RuchiDoctor.png"><br>
+<img src="../img/WhoYouAre.png">
 </div>
