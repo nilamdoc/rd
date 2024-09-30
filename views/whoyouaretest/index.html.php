b1931ce0b59a8a827035eec5508510f8eec19738
Nilam Doctor <nilamdoc@gmail.com>
2024-09-30 21:56:09 +0530

    ctp

--- views/whoyouaretest/index.html.php
+++ views/whoyouaretest/index.html.php
@@ -1,12 +1,18 @@
 <div style="background-image:url('../img/RDimage.png'); background-repeat: no-repeat;
   background-attachment: fixed;
 
-height:800" class="text-align-center ">
+height:800px" class="text-align-center ">
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <img src="../img/RuchiDoctor.png"><br><br>
 <img src="../img/WhoYouAre.png"><br><br><br><br>
-<div class="text-align-center"><form method="post" action="/whoyouaretest/reqister">
-	
-	<input type="Email" placeholder="Email" style="background-color:red; ">
-</form></div>
+<form method="post" action="/whoyouaretest/register">
+<div class="grid grid-cols-2 grid-gap">
+	<div>
+		<input type="email" placeholder="Email" style="background-color:black;border:1px solid gray;height:50px;color:white;margin:0px auto" onblur="checkEmail(this.value); return false;">
+	</div>
+	<div>
+		<input type="submit" value="Next" style="height:50px; background-color:red;color:white;border:1px solid red;width:180px;font-size:24px;font-weight:bold;cursor:pointer">
+	</div>
+</div>
+</form>
 </div>
--- webroot/js/main.js
+++ webroot/js/main.js
@@ -65,3 +65,12 @@ function goto(div){
 //	console.log(div);
 	
 }
+
+function checkEmail(email){
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};}
