985f0905c983ae76047628bfa36df5566727da26
Nilam Doctor <nilamdoc@gmail.com>
2024-09-30 19:55:48 +0530

    who

--- views/whoyouaretest/index.html.php
+++ views/whoyouaretest/index.html.php
@@ -1,5 +1,5 @@
 <div style="background-image:url('../img/RDimage.png'); background-repeat: no-repeat;
-  background-attachment: fixed;
+  background-attachment: fixed;  background-position: center;
   background-size: cover;
 height:800" class="text-align-center ">
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
